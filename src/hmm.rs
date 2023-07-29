638db74f6d3630016e0714a52bf81028ed42d7b1
Andrej Balaz <andrejbalaz001@gmail.com>
2023-07-29 21:13:50 +0200

    added initial probabilities

--- src/hmm.rs
+++ src/hmm.rs
@@ -12,28 +12,77 @@ pub struct HMM {
     pub emission: ndarray::Array2<f32>,
 }
 
+enum State {
+    StartBackground,
+    Sequence,
+    SequenceEnd,
+    Motif,
+    MotifEnd,
+    EndBackground,
+    Insert,
+}
+
 // don't use From trait on slices, unless you really mean slices
 impl From<&Vec<Module>> for HMM {
     fn from(modules: &Vec<Module>) -> Self {
-        let mut n_nodes = 0;
+        let motif_frequency = 0.001;
+
+        let mut states: Vec<State> = Vec::new();
+        states.push(State::StartBackground);
         for m in modules {
-            n_nodes += match m {
-                Module::Sequence(x) => { x.len() },
-                Module::Repeat(x) => { x.0.len() },
+            match m {
+                Module::Sequence(x) => {
+                    for _ in 0..x.len()-1 {
+                        states.push(State::Sequence);
+                    }
+                    states.push(State::SequenceEnd);
+                },
+                Module::Repeat(x) => {
+                    for _ in 0..x.0.len()-1 {
+                        states.push(State::Motif);
+                    }
+                    states.push(State::MotifEnd);
+                },
             }
         }
-        // starting background + flanks and motif + ending background + inserts
-        n_nodes = 1 + n_nodes + 1 + (n_nodes-1);
+        states.push(State::EndBackground);
+        // insert states are only between 2 non-background states (e.g. BSISISB)
+        for _ in 1..states.len()-2 {
+            states.push(State::Insert);
+        }
 
-        let mut model = HMM {
-            initial: Array::zeros(n_nodes),
-            transition: Array::zeros((n_nodes, n_nodes)),
-            emission: Array::zeros((5, n_nodes))
-        };
+        let initial = initial_probabilities(&states, motif_frequency);
+        // let transition = transition_probabilities(&mut model.transition);
+        // let emission = emission_probabilities(&mut model.emission);
 
-        model
+        HMM {
+            initial,
+            transition: Array::zeros((states.len(), states.len())),
+            emission: Array::zeros((5, states.len()))
+        }
     }
 }
+
+fn initial_probabilities(states: &Vec<State>, init: f32) -> ndarray::Array1<f32> {
+    let mut probabilities = Array::zeros(states.len());
+    let mut n = 0;
+    for (i, state) in states.iter().enumerate() {
+        match state {
+            State::Sequence | State::SequenceEnd | State::Motif | State::MotifEnd 
+                => {
+                    probabilities[[i]] = init;
+                    n += 1; 
+                },
+            _ => {},
+        }
+    }
+    probabilities[[0]] = 1.0 - (init * n as f32);
+    assert!(probabilities[[0]] >= 0.0);
+    return probabilities;
+}
+
+fn transition_probabilities(probs: &mut ndarray::Array2<f32>) {}
+fn emission_probabilities(probs: &mut ndarray::Array2<f32>) {}
 
 enum Module {
     Sequence(Vec<u8>),
@@ -66,6 +115,7 @@ mod tests {
         ];
 
         let model = HMM::from(&modules);
+        println!("{}", model.initial);
     }
 }
 
