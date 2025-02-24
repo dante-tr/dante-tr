e80ac1ec7c918f643d617f4ff44b68f50c8ad1f9
Andrej Balaz <andrejbalaz001@gmail.com>
2025-02-24 18:11:46 +0100

    added loading of relatives

--- dante_gui/src/analysis_family.rs
+++ dante_gui/src/analysis_family.rs
@@ -1,30 +1,46 @@
 use std::path::PathBuf;
+use std::path::Path;
+use native_dialog::FileDialog;
 
 use iced::alignment::{Horizontal, Vertical};
-use iced::widget::{button, checkbox, column, container, horizontal_rule, horizontal_space, row, text, tooltip, vertical_space};
-use iced::widget::Column;
+use iced::widget::{button, checkbox, column, container, horizontal_rule, horizontal_space, row, text, tooltip, vertical_space, pick_list, text_input};
+use iced::widget::{Column, Row};
 use iced::{Element, Length};
 
 use crate::{App, ContentPage};
 
 #[derive(Debug, Clone)]
-pub(crate) enum Message {
+pub(super) enum Message {
     Back,
-    Temp,
+    SetMotifs(MotifFile),
+    SetProbandSex(Sex),
+    SetRelation(usize, Relation),
+    AddRelative,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Default)]
-pub(crate) struct Data {
+pub(super) struct Data {
     pub path: PathBuf,
     pub analysis_name: String,
+    pub selected: Option<MotifFile>,
+    pub selected_file: Option<PathBuf>,
+    pub proband_bam: Option<PathBuf>,
+    pub proband_sex: Option<Sex>,
+
+    pub relatives: Vec<(Option<PathBuf>, Option<Relation>)>,
 }
 
 impl Data {
-    pub(crate) fn init(path: PathBuf, analysis_name: String) -> ContentPage {
-        ContentPage::AnalysisFamily(Data { path, analysis_name })
+    pub(super) fn init(path: PathBuf, analysis_name: String) -> ContentPage {
+        ContentPage::AnalysisFamily(Data {
+            path, analysis_name,
+            selected: None, selected_file: None,
+            proband_bam: None, proband_sex: None,
+            relatives: vec![(None, None)]
+        })
     }
 
-    pub(crate) fn view(&self) -> Element<Message> {
+    pub(super) fn view(&self) -> Element<Message> {
         println!("{:?}", self);
         let mut content = column![].align_x(Horizontal::Center).width(Length::Fill);
 
@@ -37,49 +53,37 @@ impl Data {
         return content.into();
     }
 
-    pub(crate) fn update(&mut self, m: Message) {
-        println!("{:?}\n{:?}", self, m)
+    pub(super) fn update(&mut self, m: Message) {
+        println!("{:?}\n{:?}", self, m);
+        match m {
+            Message::SetMotifs(motif_file) => { update_motif_selection(self, motif_file); },
+            Message::SetProbandSex(role) => { self.proband_sex = Some(role) },
+            Message::SetRelation(idx, role) => { println!("{:?} {:?}", idx, role); },
+            Message::AddRelative => { self.relatives.push((None, None)); }
+            Message::Back => { unreachable!() /* implemented in App::update */ }
+        }
     }
 }
 
 fn make_header(mut content: Column<Message>) -> Column<Message> {
-    content = content.push(vertical_space());
     content = content.push(row![
         container(button("Back").on_press(Message::Back)).width(100),
         container(text("Family analysis").size(App::H1_SIZE)).align_x(Horizontal::Center).width(Length::Fill),
         container("").width(100),
-    ].padding(10).align_y(Vertical::Center));
-    content = content.push(vertical_space());
+    ].padding(25).align_y(Vertical::Center));
     return content;
 }
 
 fn make_form<'a>(mut content: Column<'a, Message>, data: &'a Data) -> Column<'a, Message> {
-    content = content.push(vertical_space());
     content = content.push(row![
         container(text("Analysis name: ")).width(160).align_x(Horizontal::Right).padding(App::PAD1),
         container(text(data.analysis_name.clone())).width(Length::Fill).align_x(Horizontal::Left)
     ].padding(10).align_y(Vertical::Center));
 
-    content = content.push(row![
-        container(text("Motifs: ")).width(160).align_x(Horizontal::Right).padding(App::PAD1),
-        container(text("")).width(Length::Fill).align_x(Horizontal::Left)
-    ].padding(10).align_y(Vertical::Center));
+    content = content.push(make_motif_selection(data.selected, &data.selected_file));
+    content = content.push(make_proband_row(data));
 
-    content = content.push(row![
-        container(text("Proband: ")).width(160).align_x(Horizontal::Right).padding(App::PAD1),
-        container(text("")).width(Length::Fill).align_x(Horizontal::Left)
-    ].padding(10).align_y(Vertical::Center));
-
-    content = content.push(row![
-        container(text("Relatives: ")).width(160).align_x(Horizontal::Right).padding(App::PAD1),
-        container(text("")).width(Length::Fill).align_x(Horizontal::Left)
-    ].padding(10).align_y(Vertical::Center));
-
-    content = content.push(row![
-        container(text("")).width(160).align_x(Horizontal::Right),
-        container(text("")).width(Length::Fill).align_x(Horizontal::Left)
-    ].padding(10).align_y(Vertical::Center));
-
+    content = make_relatives(content, data);
     content = content.push(row![
         container(text("")).width(160),
         container(button("Analyze")),
@@ -87,6 +91,113 @@ fn make_form<'a>(mut content: Column<'a, Message>, data: &'a Data) -> Column<'a,
     ].padding(10).align_y(Vertical::Center));
     content = content.push(vertical_space());
     return content;
+}
+
+fn make_relatives<'a>(mut content: Column<'a, Message>, data: &'a Data) -> Column<'a, Message> {
+    let choices = [
+        Relation::Mother, Relation::Father,
+        Relation::Sister, Relation::Brother,
+        Relation::Daughter, Relation::Son,
+        Relation::Mate
+    ];
+
+    let (path, relation) = &data.relatives[0];
+    let filename = path.clone().unwrap_or(PathBuf::new()).to_string_lossy().to_string();
+    let message = |x| { Message::SetRelation(0, x) };
+    let first_row = row![
+        container(text("Relatives: ")).width(160).align_x(Horizontal::Right).padding(App::PAD1),
+        container(text_input("Type path or click search...", &filename) /* .on_input(on_input) */).padding(App::PAD1),
+        container(pick_list(choices, *relation, message).placeholder("relation")).padding(App::PAD1),
+        container(button("Search")/*.on_press(on_press)*/).padding(App::PAD1)
+    ].padding(10).align_y(Vertical::Center);
+
+    content = content.push(first_row);
+
+    for (i, (path, relation)) in data.relatives.iter().enumerate().skip(1) {
+        let filename = path.clone().unwrap_or(PathBuf::new()).to_string_lossy().to_string();
+        let message = move |x| { Message::SetRelation(i, x) };
+        let next_row = row![
+            container(text("")).width(160).align_x(Horizontal::Right).padding(App::PAD1),
+            container(text_input("Type path or click search...", &filename) /* .on_input(on_input) */).padding(App::PAD1),
+            container(pick_list(choices, *relation, message).placeholder("relation")).padding(App::PAD1),
+            container(button("Search")/*.on_press(on_press)*/).padding(App::PAD1)
+        ].padding(10).align_y(Vertical::Center);
+
+        content = content.push(next_row);
+    }
+
+    content = content.push(row![
+        container(text("")).width(160).align_x(Horizontal::Right),
+        container(button("Add relative").on_press(Message::AddRelative)).padding(App::PAD1),
+        container(text("")).width(Length::Fill).align_x(Horizontal::Left)
+    ].padding(10).align_y(Vertical::Center));
+    return content;
+}
+
+fn make_proband_row(data: &Data) -> Row<Message> {
+    let proband = match &data.proband_bam {
+        None => { "" },
+        Some(path) => { "path" }
+    };
+
+    let sex = [ Sex::Male, Sex::Female, Sex::Intersex ];
+
+    row![
+        container(text("Proband: ")).width(160).align_x(Horizontal::Right).padding(App::PAD1),
+        container(text_input("Type path or click search...", proband) /*.on_input(on_input)*/).padding(App::PAD1),
+        container(pick_list(sex, data.proband_sex, Message::SetProbandSex).placeholder("sex")).padding(App::PAD1),
+        container(button("Search")/*.on_press(on_press)*/).padding(App::PAD1)
+    ].padding(10).align_y(Vertical::Center)
+}
+
+fn make_motif_selection(selected: Option<MotifFile>, selected_file: &Option<PathBuf>) -> Row<Message> {
+    let motif_files = [
+        MotifFile::STRSet_20220902,
+        MotifFile::Custom
+    ];
+
+    let content = match selected {
+        None => {
+            row![
+                container(text("Motifs: ")).width(160).align_x(Horizontal::Right).padding(App::PAD1),
+                container(pick_list(motif_files, selected, Message::SetMotifs).placeholder("type")),
+                container(text("")).width(Length::Fill).align_x(Horizontal::Left)
+            ].padding(10).align_y(Vertical::Center)
+        }
+        Some(MotifFile::Custom) => {
+            let x: String = selected_file.clone().unwrap().to_string_lossy().to_string();
+            row![
+                container(text("Motifs: ")).width(160).align_x(Horizontal::Right).padding(App::PAD1),
+                container(pick_list(motif_files, selected, Message::SetMotifs).placeholder("type")).padding(App::PAD1),
+                container(text(x)).width(Length::Fill).align_x(Horizontal::Left)
+            ].padding(10).align_y(Vertical::Center)
+        }
+        Some(_) => {
+            row![
+                container(text("Motifs: ")).width(160).align_x(Horizontal::Right).padding(App::PAD1),
+                container(pick_list(motif_files, selected, Message::SetMotifs).placeholder("type")),
+                container(text("")).width(Length::Fill).align_x(Horizontal::Left)
+            ].padding(10).align_y(Vertical::Center)
+        }
+    };
+    return content;
+}
+
+fn update_motif_selection(data: &mut Data, motif_file: MotifFile) {
+    match motif_file {
+        MotifFile::STRSet_20220902 => {
+            /* TODO: store some path */
+            // App::DATA_DIR + "/somepath/motifs.tsv"
+            data.selected_file = Some(PathBuf::from(""));
+            data.selected = Some(motif_file);
+        }
+        MotifFile::Custom => {
+            if let Ok(Some(x)) = FileDialog::new().show_open_single_file() {
+                data.selected_file = Some(x);
+                data.selected = Some(motif_file);
+            }
+        }
+    }
 }
 
 fn make_report<'a>(mut content: Column<'a, Message>, data: &'a Data) -> Column<'a, Message> {
@@ -123,3 +234,63 @@ fn make_report<'a>(mut content: Column<'a, Message>, data: &'a Data) -> Column<'
     content = content.push(vertical_space());
     return content;
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub(super) enum MotifFile {
+    #[allow(non_camel_case_types)]
+    STRSet_20220902,
+    Custom,
+}
+
+impl std::fmt::Display for MotifFile {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(match self {
+            Self::STRSet_20220902 => "STRSet_20220902",
+            Self::Custom => "custom",
+        })
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub(super) enum Relation {
+    Mother,
+    Father,
+    Sister,
+    Brother,
+    Daughter,
+    Son,
+    Mate,
+}
+
+impl std::fmt::Display for Relation {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(match self {
+            Self::Mother => "mother",
+            Self::Father => "father",
+            Self::Sister => "sister",
+            Self::Brother => "brother",
+            Self::Daughter => "daughter",
+            Self::Son => "son",
+            Self::Mate => "mate",
+        })
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub(super) enum Sex {
+    Male,
+    Female,
+    Intersex,
+}
+
+impl std::fmt::Display for Sex {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(match self {
+            Self::Male => "male",
+            Self::Female => "female",
+            Self::Intersex => "intersex"
+        })
+    }
+}
+
+
